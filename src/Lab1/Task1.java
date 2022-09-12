3cdd755c2bc0d5577e10278fcc77a8bd624584cc
Nerdan0 <01castle02@gmail.com>
2022-09-12 18:45:39 +0300

    1

---
+++ src/Lab1/Task1.java
@@ -0,0 +1,53 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int a,b,x,y;
+
+        a = in.nextInt();
+        b = in.nextInt();
+        x = in.nextInt();
+        y = in.nextInt();
+
+        if (a<x)
+        {
+            if (b<x)
+            {
+                System.out.println(0.0);
+            } else if (b>=x) {
+                if (b>y)
+                {
+                    //perviy levee, nakladivayutsa krayami
+                    System.out.println((float)(Math.abs(y-x)+1)/(float)(Math.abs(b-a)+1));
+                }
+                else
+                {
+                    //perviy bolshe vtorogo
+                    System.out.println((float)(Math.abs(b-x)+1)/(float)(Math.abs(y-x)+1));
+                }
+            }
+        }
+        else if (x<a)
+        {
+            if (y<a)
+            {
+                System.out.println(0.0);
+            } else if (y>=a) {
+                if (y>b)
+                {
+                    //perviy levee, nakladivayutsa krayami
+                    System.out.println((float)(Math.abs(b-a)+1)/(float)(Math.abs(y-x)+1));
+                }
+                else
+                {
+                    //perviy bolshe vtorogo
+                    System.out.println((float)(Math.abs(y-x)+1)/(float)(Math.abs(y-x)+1));
+                }
+            }
+        }
+    }
+}
